1e7b40b8f3f68a7379bbb0c6e02a8658ff3167de
gousban <gousban@webex.cloudns.be>
2025-03-25 16:40:02 +0800

    Removedd unused nodeIndex

--- speedtest.go
+++ speedtest.go
@@ -77,7 +77,7 @@ func tcpTest(cfg types.Config, nodes []types.Proxy, testLogger *log.Logger) []ty
 		testLogger.Printf("Node %d: Switching to %s:%d (%s)", i, node.Server, node.Port, node.Name)
 
 		// Switch to the current node using the API
-		err := switchNode(client, node, i)
+		err := switchNode(client, node)
 		if err != nil {
 			testLogger.Printf("Node %d: Failed to switch to %s:%d (%s): %v", i, node.Server, node.Port, node.Name, err)
 			failedCount++
@@ -165,7 +165,7 @@ func tcpTest(cfg types.Config, nodes []types.Proxy, testLogger *log.Logger) []ty
 	return testedNodes
 }
 
-func switchNode(client command.HandlerServiceClient, node types.Proxy, nodeIndex int) error {
+func switchNode(client command.HandlerServiceClient, node types.Proxy) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
